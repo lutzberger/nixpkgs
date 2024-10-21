8e9261d45fd10302ce0da176b9c03e624c9d0ef7
Lutz Berger <lutz.berger@airbus.com>
2024-10-21 10:33:43 +0200

    florist: init at 24.2

---
+++ pkgs/by-name/fl/florist/package.nix
@@ -0,0 +1,36 @@
+{
+  stdenv,
+  gnat13,
+  gnat13Packages,
+  fetchFromGitHub,
+  lib,
+}:
+
+stdenv.mkDerivation rec {
+  name = "florist";
+  version = "24.2";
+  src = fetchFromGitHub {
+    owner = "adacore";
+    repo = "florist";
+    rev = "refs/heads/${version}";
+    hash = "sha256-EFGmcQfWpxEWfsAoQrHegTlizl6siE8obKx+fCpVwUQ=";
+  };
+
+  configurePhase = ''
+    ./configure --enable-shared --prefix=$out
+  '';
+
+  nativeBuildInputs = [
+    gnat13
+    gnat13Packages.gprbuild
+  ];
+
+  meta = {
+    description = "Posix Ada Bindings";
+    homepage = "https://github.com/adacore/florist";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ lutzberger ];
+    platforms = lib.platforms.linux;
+  };
+
+}
